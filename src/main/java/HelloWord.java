d41fc6397b812d17a2bf4df50c2abe7404292e40
taowf <taowf@133.cn>
2020-08-13 16:02:12 +0800

    issue04

--- src/main/java/HelloWord.java
+++ src/main/java/HelloWord.java
@@ -6,6 +6,8 @@ public class HelloWord {
     String b = "hello world";
 
     public static void main(String[] args){
+
         System.out.println("hello world");
+        System.out.println("1234");
     }
 }
